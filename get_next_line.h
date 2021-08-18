a0c43f11e692c4a667602895e72a8da3ff4cd9e4
denisgodoy <denisgodoy1@gmail.com>
2021-08-18 12:26:51 -0300

    update gnl

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/18 12:24:47 by degabrie          #+#    #+#             */
+/*   Updated: 2021/08/18 12:24:48 by degabrie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int	fd);
+
+#endif
