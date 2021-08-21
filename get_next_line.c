818262c74053a5e5b8c9cbf84f9023e84a412fb7
denisgodoy <denisgodoy1@gmail.com>
2021-08-21 14:05:46 -0300

    update gnl

--- get_next_line.c
+++ get_next_line.c
@@ -15,8 +15,8 @@
 char	*get_next_line(int	fd)
 {
 	static char	*buffer;
-	char	*line;
-	char	*strline;
+	char		*line;
+	char		*strline;
 
 	if (fd > 0)
 	{
@@ -24,37 +24,13 @@ char	*get_next_line(int	fd)
 		if (!line)
 			return (0);
 		if (!buffer)
+		{
 			buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
 			if (!buffer)
 				return (0);
-
+		}
 		return (strline);
 	}
 	return (0);
 }
 
-int main(void)
-{
-	char	*str;
-	int		fd;
-	int		fd2;
-	int		i;
-
-    fd = open("text.txt", O_RDONLY);
-    fd2 = open("text2.txt", O_RDONLY);
-	str = get_next_line(fd);
-	printf("%d\n", fd);
-	printf("%d\n", fd2);
-	printf("%s\n", str);
-	i = 0;
-	while (str[i] != '\0')
-	{
-		printf("%c\n", str[i]);
-		printf("%p\n", str);
-		i++;
-	}
-	close(fd);
-	close(fd2);
-	free(str);
-	return (0);
-}
