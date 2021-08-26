98fbcba80a4497003bfaa1b2b8aaa450199e859b
denisgodoy <denisgodoy1@gmail.com>
2021-08-26 19:30:05 -0300

    update gnl

--- get_next_line.c
+++ get_next_line.c
@@ -5,65 +5,49 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/19 22:11:33 by degabrie          #+#    #+#             */
-/*   Updated: 2021/08/26 00:17:31 by degabrie         ###   ########.fr       */
+/*   Created: 2021/08/26 15:28:21 by degabrie          #+#    #+#             */
+/*   Updated: 2021/08/26 19:25:31 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"get_next_line.h"
-#include	"libft/libft.h"
-
-static char	*ft_read_line(char	*line, char	*leaks, int	fd);
-static char	*ft_leak_backup(char	*line, char	*leaks);
 
 char	*get_next_line(int	fd)
 {
+	static char	*remaining;
+	ssize_t		bytes;
+	size_t		flag;
 	char		*line;
-	static char	*leaks;
+	char		buffer[BUFFER_SIZE + 1];
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd >= 256)
 		return (0);
-	line = ft_strdup("");
-	if (leaks)
+	else if (remaining)
 	{
-		line = ft_read_line(line, leaks, fd);
-		free(leaks);
-		if (ft_strchr(line, '\n'))
-			leaks = ft_leak_backup(line, leaks);
-		line = ft_substr(line, 0, ft_strlen(line) - ft_strlen(leaks));
-		return (line);
+		line = ft_strdup("");
+		line = ft_strdup(remaining);
+		free(remaining);
 	}
-	line = ft_read_line(line, leaks, fd);
-	leaks = ft_leak_backup(line, leaks);
-	line = ft_substr(line, 0, ft_strlen(line) - ft_strlen(leaks));
-	return (line);
-}
-
-static char	*ft_read_line(char	*line, char	*leaks, int	fd)
-{
-	char	buffer[BUFFER_SIZE + 1];
-	ssize_t	bytes_read;
-	size_t	found_n;
-
-	bytes_read = 1;
-	found_n = 1;
-	while (found_n && bytes_read)
+	else
+		line = ft_strdup("");
+	bytes = 1;
+	flag = 1;
+	while (flag && bytes)
 	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (ft_strchr(line, '\n'))
-			found_n = 0;
-		buffer[bytes_read] = '\0';
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		buffer[bytes] = '\0';
 		line = ft_strjoin(line, buffer);
+		if (ft_strchr(line, '\n'))
+		{
+			remaining = ft_strdup(ft_strchr(line, '\n') + 1);
+			line = ft_substr(line, 0, ft_strlen(line) - ft_strlen(remaining));
+			flag = 0;
+		}
+		if (!bytes)
+		{
+			free(line);
+			return(0);
+		}
 	}
-	if (leaks)
-		line = ft_strjoin(leaks, line);
 	return (line);
-}
-
-static char	*ft_leak_backup(char	*line, char	*leaks)
-{
-	leaks = ft_strdup(ft_strchr(line, '\n') + 1);
-	if (!leaks)
-		free(leaks);
-	return (leaks);
 }
