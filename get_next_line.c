053da8f8e35fa696507160038b310f5dc6900f46
denisgodoy <denisgodoy1@gmail.com>
2021-08-27 20:24:36 -0300

    update gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 15:28:21 by degabrie          #+#    #+#             */
-/*   Updated: 2021/08/26 22:54:16 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/08/27 20:22:50 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,14 @@ char	*get_next_line(int	fd)
 	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 256)
 		return (0);
 	if (remaining)
+	{
 		line = ft_strdup(remaining);
+		free(remaining);
+	}
 	else
+	{
 		line = ft_strdup("");
+	}
 	bytes = 1;
 	while (bytes)
 	{
@@ -42,13 +47,14 @@ char	*get_next_line(int	fd)
 		if (ft_strchr(line, '\n'))
 		{
 			temp_line = line;
-			remaining = ft_strdup(ft_strchr(temp_line, '\n') + 1);
-			line = ft_substr(line, 0, ft_strlen(temp_line) - ft_strlen(remaining));
+			remaining = (char *)malloc(ft_strlen(temp_line) + 1 * sizeof(char));
+			ft_strlcpy(remaining, ft_strchr(temp_line, '\n') + 1, ft_strlen(temp_line));
+			line = ft_substr(temp_line, 0, ft_strlen(temp_line) - ft_strlen(remaining));
 			free(temp_line);
 			break ;
 		}
 	}
-	if (!bytes && line[0] == '\0')
+	if (!bytes && line[bytes] == '\0')
 	{
 		free(line);
 		return (0);
