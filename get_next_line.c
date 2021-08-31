2c12122b92b7b39ebae0e84fd0c755e460139157
denisgodoy <denisgodoy1@gmail.com>
2021-08-31 19:46:55 -0300

    update gnl

--- get_next_line.c
+++ get_next_line.c
@@ -5,54 +5,84 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/26 15:28:21 by degabrie          #+#    #+#             */
-/*   Updated: 2021/08/30 16:22:08 by degabrie         ###   ########.fr       */
+/*   Created: 2021/08/19 22:11:33 by degabrie          #+#    #+#             */
+/*   Updated: 2021/08/31 19:36:29 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"get_next_line.h"
 
+static char	*ft_read_line(char	*line, char	*leaks, int	fd);
+static char	*ft_check_eof(ssize_t bytes, char	*line, char	*leaks);
+
 char	*get_next_line(int	fd)
 {
-	static char	remaining[BUFFER_SIZE + 1];
-	ssize_t		bytes;
 	char		*line;
-	char		*temp_line;
-	char		*temp_remaining;
-	char		buffer[BUFFER_SIZE + 1];
+	static char	leaks[BUFFER_SIZE];
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	line = ft_strdup(remaining);
-	bytes = 1;
-	while (bytes)
+	line = ft_strdup(leaks);
+	line = ft_read_line(line, &*leaks, fd);
+	return (line);
+}
+
+static char	*ft_read_line(char	*line, char	*leaks, int	fd)
+{
+	char	buffer[BUFFER_SIZE + 1];
+	ssize_t	bytes_read;
+	char	*temp_leak;
+
+	bytes_read = 1;
+	while (bytes_read)
 	{
-		bytes = read(fd, buffer, BUFFER_SIZE);
-		if (bytes < 0)
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
 		{
 			free(line);
 			return (0);
 		}
-		buffer[bytes] = '\0';
-		temp_line = line;
-		line = ft_strjoin(temp_line, buffer);
-		free(temp_line);
+		buffer[bytes_read] = '\0';
+		line = ft_strjoin(line, buffer);
 		if (ft_strchr(line, '\n'))
 		{
-			temp_line = line;
-			temp_remaining = ft_strchr(line, '\n') + 1;
-			ft_strlcpy(remaining, temp_remaining, ft_strlen(temp_remaining) + 1);
-			line = ft_substr(temp_line, 0, ft_strlen(temp_line) - ft_strlen(remaining));
-			free(temp_line);
+			temp_leak = ft_strchr(line, '\n') + 1;
+			ft_strlcpy(&*leaks, temp_leak, ft_strlen(temp_leak) + 1);
+			line = ft_substr(line, 0, ft_strlen(line) - ft_strlen(&*leaks));
 			break ;
 		}
 	}
+	return (ft_check_eof(bytes_read, line, &*leaks));
+}
+
+static char	*ft_check_eof(ssize_t bytes, char	*line, char	*leaks)
+{
 	if (!ft_strchr(line, '\n'))
-		ft_memset(&remaining, 0, ft_strlen(remaining));
+		ft_memset(&*leaks, 0, ft_strlen(&*leaks));
 	if (!bytes && line[bytes] == '\0')
 	{
 		free(line);
 		return (0);
 	}
 	return (line);
+}
+
+void	*ft_memset(void	*b, int	c, size_t	len)
+{
+	char	*str;
+
+	str = (char *)b;
+	while (len--)
+		*(str++) = (unsigned char)c;
+	return (b);
+}
+
+size_t	ft_strlen(const char	*s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/18 12:24:47 by degabrie          #+#    #+#             */
-/*   Updated: 2021/08/18 12:24:48 by degabrie         ###   ########.fr       */
+/*   Created: 2021/08/19 22:11:00 by degabrie          #+#    #+#             */
+/*   Updated: 2021/08/31 19:39:25 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+void	*ft_memset(void	*b, int	c, size_t	len);
+char	*ft_strchr(const char	*s, int	c);
+char	*ft_strdup(const char	*s1);
+char	*ft_strjoin(char	*s1, char const	*s2);
+size_t	ft_strlcpy(char	*dst,	const char	*src,	size_t	dstsize);
+size_t	ft_strlen(const char	*s);
+char	*ft_substr(char	*s, unsigned int	start, size_t	len);
 char	*get_next_line(int	fd);
 
 #endif
